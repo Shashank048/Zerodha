9ae49d234296a76ea162806f35d9efd29282b184
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-22 17:52:06 +0530

    Update Holdings.js

--- dashboard/src/component/Holdings.js
+++ dashboard/src/component/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("https://zerodha-five.vercel.app/allHoldings").then((res) => {
+    axios.get("https://zerodha-7nh0.onrender.com//allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });

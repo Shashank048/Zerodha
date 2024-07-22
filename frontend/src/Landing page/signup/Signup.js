a0c4b73ee525a94dad242c44d79bec66ad7d846f
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-22 23:13:23 +0530

    Update Signup.js

--- frontend/src/Landing page/signup/Signup.js
+++ frontend/src/Landing page/signup/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-function Signup() {
+class Signup extends Component() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);

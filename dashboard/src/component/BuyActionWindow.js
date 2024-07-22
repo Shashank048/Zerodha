9552056d5555b08ebb450a03dc00e4015c0ef6ff
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-22 15:22:22 +0530

    Update BuyActionWindow.js

--- dashboard/src/component/BuyActionWindow.js
+++ dashboard/src/component/BuyActionWindow.js
@@ -13,7 +13,7 @@ const BuyActionWindow = ({ uid }) => {
   const { closeBuyWindow } = useContext(GeneralContext);
 
   const handleBuyClick = () => {
-    axios.post("hhttps://zerodha-nu.vercel.app/AllHolding", {
+    axios.post("https://zerodha-five.vercel.app/AllHolding", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,

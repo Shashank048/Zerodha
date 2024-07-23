fb19a9b19a137036706053191ae81f4575b17c8b
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-23 11:57:50 +0530

    Update Signup.js

--- frontend/src/Landing page/signup/Signup.js
+++ frontend/src/Landing page/signup/Signup.js
@@ -56,7 +56,7 @@ function Signup() {
         <div className="col-4 p-5">
           <h1>Signup now </h1>
           <p className="text-muted">Or track your existing application.</p>
-          <form id="signup-form" action="/signup" method="post" onSubmit={handleSubmit}>
+          <form id="signup-form"  onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="exampleInputEmail1">Email address : </label>
               &nbsp;&nbsp;&nbsp;&nbsp;

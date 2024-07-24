156866b718a76e9e92d655918c417897e550039c
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-24 12:39:51 +0530

    Update Education.js

--- frontend/src/Landing page/home/Education.js
+++ frontend/src/Landing page/home/Education.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import "./Award.css"
 
 function Education() {
     return ( 
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6'>
+                <div className='col-6'class="educa-a">
                     <img src='media/education.svg' style={{width:"70%"}}/>
                 </div>
-                <div className='col-4'>
+                <div className='col-4'class="educa-b">
                     <h1 className='mb-3 fs-2'>Free and open market education</h1>
                     <p>Varsity, the largest online stock market education book in the world 
                     covering everything from the basics to advanced trading.</p>

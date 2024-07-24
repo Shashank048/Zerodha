4a212e97783d3c9306d9cbe6a7567b28bef7dfe4
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-24 18:17:28 +0530

    Update Education.js

--- frontend/src/Landing page/home/Education.js
+++ frontend/src/Landing page/home/Education.js
@@ -5,10 +5,10 @@ function Education() {
     return ( 
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6'class="educa-a">
+                <div className='col-6 educa-a'>
                     <img src='media/education.svg' style={{width:"70%"}}/>
                 </div>
-                <div className='col-4'class="educa-b">
+                <div className='col-4 educa-b>
                     <h1 className='mb-3 fs-2'>Free and open market education</h1>
                     <p>Varsity, the largest online stock market education book in the world 
                     covering everything from the basics to advanced trading.</p>

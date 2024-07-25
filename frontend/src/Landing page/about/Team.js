571558dc40d14076896dea050edef928d6e735fd
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-25 23:26:12 +0530

    Update Team.js

--- frontend/src/Landing page/about/Team.js
+++ frontend/src/Landing page/about/Team.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./all.css"
 
 function Team() {
   return (
@@ -10,12 +11,12 @@ function Team() {
         </h1>
       </div>
       <div className="row p-3 text-muted" style={{lineHeight:"1.8", fontSize:"1.0em"}}>
-        <div className="col-6 p-5 text-center">
+        <div className="az col-6 p-5 text-center">
          <img src='media/nithinKamath.jpg'style={{borderRadius:"100%",width:"50%"}}/>
          <h4 className='mt-5'>Nithin Kamath</h4>
          <h7 className='mt-3'>Founder, CEO</h7>
         </div>
-        <div className="col-6 p-5">
+        <div className="za col-6 p-5">
             <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the <br/> hurdles he faced during his decade long stint as a trader. Today,<br/> Zerodha has changed the landscape of the Indian broking industry.</p>
             <p>He is a member of the SEBI Secondary Market Advisory Committee <br/> (SMAC) and the Market Data Advisory Committee (MDAC).</p>
             <p>Playing basketball is his zen.</p>

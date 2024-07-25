a4371c6d0598b11c467c3ae1541abd2a2a65bc8d
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-25 16:45:25 +0530

    Update OpenAccount.js

--- frontend/src/Landing page/OpenAccount.js
+++ frontend/src/Landing page/OpenAccount.js
@@ -1,15 +1,28 @@
-import React from 'react'
+openAccount.js import React from "react";
 
 function OpenAccount() {
-    return ( 
-        <div className='container p-9 mt-5'>
-         <div className='row text-center p-9 mb-6'>
-           <h1 className='mt-5'>Open a Zerodha account</h1>
-           <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-           <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}><a href="/signup"/> Sign up Now </button>
-         </div>
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <div className="col-12">
+          <h1 className="mt-5">Open a Zerodha account</h1>
+          <p>
+            Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+            F&O trades.
+          </p>
+          <button
+            className="btn btn-primary fs-5 mb-5"
+            style={{ width: "60%", maxWidth: "300px", margin: "0 auto" }}>
+            <a
+              href="/signup"
+              style={{ textDecoration: "none", color: "white" }}>
+              Sign up Now
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
-     );
+  );
 }
 
 export default OpenAccount;

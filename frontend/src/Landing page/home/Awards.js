4f1e02ba0958e30ef4992835d514cfe78698bc6b
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-25 17:05:57 +0530

    Update Awards.js

--- frontend/src/Landing page/home/Awards.js
+++ frontend/src/Landing page/home/Awards.js
@@ -1,52 +1,60 @@
-import React from 'react'
+import React from "react";
 import "./Award.css";
 
-
 function Awards() {
-    return ( 
-        <div className='container mt-5 sa'>
-            <div className='row'>
-                <div className='col-6 p-5'>
-                    <img src='media/largestBroker.svg'/>
-                </div>
-                <div className='col-6 p-5 mt-5'>
-                    <h1>Largest stock broker in India</h1>
-                    <p className='mb-5'> 2+ million Zerodha clients contribute to over 15% of all retail order
-                    volumes in India daily by trading and investing in:
-                    </p>
-                    <div className='row'>
-                        <div className='col-6'>
-                            <ul>
-                                 <li>
-                                    <p>Futures and Options</p>
-                                 </li>
-                                 <li>
-                                    <p>Commodity derivatives</p>
-                                 </li>
-                                 <li>
-                                    <p>Currency derivatives</p>
-                                 </li>
-                            </ul>
-                        </div>
-                        <div className='col-6'>
-                            <ul>
-                                 <li>
-                                    <p>Stocks & IPOs</p>
-                                 </li>
-                                 <li>
-                                    <p>Direct mutual funds</p>
-                                 </li>
-                                 <li>
-                                    <p>Bonds and Govt. Securities</p>
-                                 </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <img src='media/pressLogos.png' style={{width:"90%"}}/>
-                </div>
-            </div>
+  return (
+    <div className="container mt-5 sa">
+      <div className="row">
+        <div className="col-12 col-md-6 p-5 text-center">
+          <img
+            src="media/largestBroker.svg"
+            className="img-fluid"
+            alt="Largest Broker"
+          />
         </div>
-     );
+        <div className="col-12 col-md-6 p-5 mt-5 mt-md-0">
+          <h1>Largest stock broker in India</h1>
+          <p className="mb-5">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in India daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img
+            src="media/pressLogos.png"
+            className="img-fluid mt-4"
+            alt="Press Logos"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;

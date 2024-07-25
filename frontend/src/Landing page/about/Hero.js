2884ad6d00c000b3d7ec60b7e7b799b7a0dd59e3
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-25 23:18:18 +0530

    Update Hero.js

--- frontend/src/Landing page/about/Hero.js
+++ frontend/src/Landing page/about/Hero.js
@@ -4,7 +4,7 @@ import "./all.css";
 function Hero() {
   return (
     <div className="container">
-      <div className="row p-5 mt-5 mb-5">
+      <div className="row p-5 mt-5 mb-4">
         <h1 className="fs-2 text-center">
           We pioneered the discount broking model in India.<br/> Now, we are breaking
           ground with our technology.

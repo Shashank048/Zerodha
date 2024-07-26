42116eae62a9bd695e7b7e1f7d532a8ddc3253aa
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 19:08:09 +0530

    Update Hero.js

--- frontend/src/Landing page/products/Hero.js
+++ frontend/src/Landing page/products/Hero.js
@@ -3,10 +3,10 @@ import React from "react";
 function Hero() {
   return (
     <div className="container border-bottom mb-5">
-      <div className="text-center mt-5 p-5">
-        <h1> Technology</h1>
-        <h3 className="text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
-        <p className="mt-3 mb-5">
+      <div className="p text-center mt-5 p-5">
+        <h1 className="pb"> Technology</h1>
+        <h3 className="pc text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
+        <p className="pd mt-3 mb-5">
           Check out our{" "}
           <a href="" style={{ textDecoration: "none" }}>
             investment offerings →

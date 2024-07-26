249ed728e94438eab2e6658fc38e8f890c1a5932
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 20:57:04 +0530

    Update Team.js

--- frontend/src/Landing page/about/Team.js
+++ frontend/src/Landing page/about/Team.js
@@ -11,7 +11,7 @@ function Team() {
         </h1>
       </div>
       <div className="row p-3 text-muted" style={{lineHeight:"1.8", fontSize:"1.0em"}}>
-        <div className="az col-6 p-5 text-center">
+        <div className="az col-lg-6 p-5 text-center">
          <img src='media/nithinKamath.jpg'style={{borderRadius:"100%",width:"50%"}}/>
          <h4 className='mt-5'>Nithin Kamath</h4>
          <h7 className='mt-3'>Founder, CEO</h7>

a0bb8d8bf617c5148448cb5ae7d3f6c94b99707b
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 19:51:22 +0530

    Update RightSection.js

--- frontend/src/Landing page/products/RightSection.js
+++ frontend/src/Landing page/products/RightSection.js
@@ -4,14 +4,14 @@ function RightSection({imageURL, productName, productDescripion, learnMore}) {
   return (
     <div className="container mt-5">
         <div className="row">
-            <div className="col-6 p-5 mt-5">
-                <h1>{productName}</h1>
-                <p>{productDescripion}</p>
+            <div className="pf col-6 p-5 mt-5">
+                <h1 className="pg">{productName}</h1>
+                <p className="ph">{productDescripion}</p>
                 <div>
                    <a href={learnMore} style={{textDecoration:"none"}}>Learn more →</a>
                 </div>
             </div>
-            <div className="col-6">
+            <div className="pi col-6">
                    <img src={imageURL}/>
               </div>
         </div>

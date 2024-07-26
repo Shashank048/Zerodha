d216b1bd2e75843c9689ec2636f90fd4399acc24
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 23:55:35 +0530

    Update Hero.js

--- frontend/src/Landing page/support/Hero.js
+++ frontend/src/Landing page/support/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className="container-fluid w-full" id="supportHero">
       <div className="p-5 me-3 d-flex justify-content-between align-items-center" id="supportWrapper">
-        <h4 class=" p-3 ">Support Portal</h4>
+        <h4 class=" p-3 margin-right: 1rem">Support Portal</h4>
         <a href="" style={{ textDecoration: "none" }}>
           Track Tickets
         </a>

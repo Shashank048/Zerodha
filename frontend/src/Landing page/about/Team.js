72645a335e3bc9142ef45330400798f4f1244524
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 20:36:57 +0530

    Update Team.js

--- frontend/src/Landing page/about/Team.js
+++ frontend/src/Landing page/about/Team.js
@@ -16,7 +16,7 @@ function Team() {
          <h4 className='mt-5'>Nithin Kamath</h4>
          <h7 className='mt-3'>Founder, CEO</h7>
         </div>
-        <div className="za col-6 p-5">
+        <div className="za col-lg-6 p-5">
             <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the <br/> hurdles he faced during his decade long stint as a trader. Today,<br/> Zerodha has changed the landscape of the Indian broking industry.</p>
             <p>He is a member of the SEBI Secondary Market Advisory Committee <br/> (SMAC) and the Market Data Advisory Committee (MDAC).</p>
             <p>Playing basketball is his zen.</p>

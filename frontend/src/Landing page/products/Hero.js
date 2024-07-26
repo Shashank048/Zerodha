548498eb6c7615d2b1d32ce050b2e299ac8e42d2
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-07-26 19:10:13 +0530

    Update Hero.js

--- frontend/src/Landing page/products/Hero.js
+++ frontend/src/Landing page/products/Hero.js
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <div className="container border-bottom mb-5">
       <div className="p text-center mt-5 p-5">
-        <h1 className="pb"> Technology</h1>
-        <h3 className="pc text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
-        <p className="pd mt-3 mb-5">
+        <h1 className="pa"> Technology</h1>
+        <h3 className="pb text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
+        <p className="pc mt-3 mb-5">
           Check out our{" "}
           <a href="" style={{ textDecoration: "none" }}>
             investment offerings →
